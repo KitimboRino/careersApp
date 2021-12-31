9ac180eb1393300d1ea6237333c286bd3ead26da
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-01-01 00:50:22 +0300

    Clean up

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlQHRCubRbU-wq-pNA0yaVUG_HD5jpfjA",
+    authDomain: "career-firebase-app-5a310.firebaseapp.com",
+    projectId: "career-firebase-app-5a310",
+    storageBucket: "career-firebase-app-5a310.appspot.com",
+    messagingSenderId: "429140093777",
+    appId: "1:429140093777:web:c72b503f378855188fc170",
+    measurementId: "G-Q07B1XXC9N"
+  };
